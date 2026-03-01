45cd118f6a805c1a4921b9bfbcd71907a11775bd
ScaleScribe <scalescribe.mythicrealms@gmail.com>
2026-03-01 15:04:22 -0600

    Delete functions directory

--- functions/campaigns/[slug].ts
+++
@@ -1,5 +0,0 @@
-export const onRequest: PagesFunction = async ({ request }) => {
-  const url = new URL(request.url);
-  url.pathname = "/campaigns/_/index.html";
-  return fetch(new Request(url.toString(), request));
-};
